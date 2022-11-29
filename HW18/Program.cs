feadd69f67514a19fee4bbc9eaf150cec126360d
KyryloStroiev <stroievkyrylo@gmail.com>
2022-11-29 21:10:31 +0200

    Add project files.

---
+++ HW18/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+using System.Reflection;
+
+Assembly assemblys = Assembly.LoadFrom("C:\\Users\\User\\Desktop\\HM\\HW18\\MyLibrary\\MyAssembly.cs");
+Type t = assemblys.GetType("MyLibrary.MyAssembly");
+var metods = t.GetMethod("Metod", new Type[] {typeof(int), typeof(int)});
+if (metods != null)
+{
+    throw new Exception();
+}
+object[] constructorParameters = new object[2];
+constructorParameters[0] = 150;
+constructorParameters[1] = 5;
+
+var obj = Activator.CreateInstance(t, constructorParameters);
+
+object[] parameters = new object[2];
+parameters[0] = 2342;
+parameters[1] = 125;
+
+var records = metods.Invoke(obj, parameters);
+Console.WriteLine(records);
---
+++ MyLibrary/MyAssembly.cs
@@ -0,0 +1,21 @@
+﻿namespace MyLibrary;
+
+public class MyAssembly
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+    public MyAssembly(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+   public int Metod (int x, int y)
+    {
+        return x * y;
+    }
+    public static void MetodB(int x, int y)
+    {
+        Console.WriteLine($"Hello {x} {y} ");
+    }
+
+}
